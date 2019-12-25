0cc6860a299239dd234224ff2f8fdc7cd5378c3e
Wyatt <p@hty.email>
2019-12-25 14:51:11 +0800

    add setting function

---
+++ underdevelop/GUI/setting.py
@@ -0,0 +1,29 @@
+#encoding:gbk
+
+import json
+
+def get_setting():
+    with open('config.evlp','r') as f:
+        data = json.loads(f.read())
+        f.close()
+        return data
+
+def addMoney(money):
+    current_setting = get_setting()
+    current_setting['money'] += money
+    with open('config.evlp','w') as f:
+        f.write(json.dumps(current_setting))
+    f.close()
+
+'''
+def write_setting(update_frequency, group_name):
+    money =  get_setting()['money']
+    with open('config.evlp','w') as f:
+        f.write(json.dumps({
+            'update':update_frequency,
+            'group':group_name,
+            'money':money
+        }))
+        f.close()
+'''
+
